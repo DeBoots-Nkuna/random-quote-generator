b5b1ed271c478d02fd488c169d153cdf5b690d38
DE-BOOTS <123546993+DE-BOOTS@users.noreply.github.com.>
2025-01-22 11:55:32 +0200

    "first Commit" "simple random quote generator"

---
+++ src/assets/data.js
@@ -0,0 +1,51 @@
+//sample data of quotes collected in goodreads.com
+//https://goodreads.com/quotes
+
+const QUOTES = [
+  {
+    author: '― Oscar Wilde',
+    quote: 'Be yourself; everyone else is already taken',
+  },
+  {
+    author: '― Marilyn Monroe',
+    quote:
+      'Im selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you cant handle me at my worst, then you sure as hell dont deserve me at my best.',
+  },
+  {
+    author: '― Albert Einstein',
+    quote:
+      'Two things are infinite: the universe and human stupidity; and Im not sure about the universe.',
+  },
+  {
+    author: '― Bernard M. Baruch',
+    quote:
+      'Be who you are and say what you feel, because those who mind dont matter, and those who matter dont mind.',
+  },
+  {
+    author: '― Mae West',
+    quote: 'You only live once, but if you do it right, once is enough.',
+  },
+  {
+    author: '― Mahatma Gandhi',
+    quote: 'Be the change that you wish to see in the world.',
+  },
+  {
+    author: '― Robert Frost',
+    quote:
+      'In three words I can sum up everything Ive learned about life: it goes on.',
+  },
+  {
+    author: '',
+    quote:
+      'Dont walk in front of me… I may not follow Dont walk behind me… I may not lead Walk beside me… just be my friend',
+  },
+  {
+    author: '― Mark Twain',
+    quote: 'If you tell the truth, you dont have to remember anything.',
+  },
+  {
+    author: '― Ralph Waldo Emerson',
+    quote:
+      'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment',
+  },
+];
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">Random Quote Generator</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
